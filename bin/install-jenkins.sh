c896ff3776b599930f3050f7c2f96376421922e3
Thiago Ádril <thiago.adril@opentechgr.com.br>
2020-03-16 14:10:48 -0300

    updated

--- bin/install-jenkins.sh
+++ bin/install-jenkins.sh
@@ -11,8 +11,8 @@ echo "Installing jenkins on docker..."
 DOCKER_COMPOSE_EXE=$(which docker-compose)
 
 if [ -f $DOCKER_COMPOSE_EXE ]; then
-    docker image build -f env/jenkins/Dockerfile -t jenkins:latest
-    docker-compose -f env/jenkins/docker-compose.yaml up -d
+    docker image build ./env/jenkins -t jenkins:latest
+    docker-compose -f ./env/jenkins/docker-compose.yaml up -d
 else
     echo "Docker compose not instaled."
 fi
